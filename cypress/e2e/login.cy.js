03896c2a8468c84766e3e427b601b2a7ca385949
martemoslet <martemoslet@gmail.com>
2023-01-26 13:15:55 +0100

    Updates login e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,11 +14,13 @@ describe("Social Media App: Authenticated user", () => {
     it("user can login", () => {
         cy.get("#loginForm").should("be.visible");
         cy.get("#loginEmail").should("be.visible").type(`marte.moslet@noroff.no`, {delay: 100});
-        cy.get("#loginPassword").should("exist").type(`passord1{enter}`, {delay: 100});
+        cy.get("#loginPassword").should("exist").type(`passord1{enter}`).should(() => {
+            expect(localStorage.getItem("token")).to.not.be.null
+        });
 
         cy.url().should('include', 'profile');
         cy.url().should('not.include', 'login');
-        expect(localStorage.getItem("token")).to.not.be.null
+        
     })
 
 
