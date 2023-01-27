049c42fcf5ec1989d4d8ba43192705aa3dd3d38e
martemoslet <martemoslet@gmail.com>
2023-01-27 14:22:43 +0100

    Updates create test

--- src/js/api/posts/create.test.js
+++ src/js/api/posts/create.test.js
@@ -1,22 +1,25 @@
 import { createPost } from "./create";
 
 const mockpost = {
-    title: "title",
-    tags: ["tag1", "tag2"],
-    media: "",
-    body: "body"
+  title: "title",
+  tags: ["tag1", "tag2"],
+  media: "",
+  body: "body",
 };
 
-const mockfetch = jest.fn().mockResolvedValue({
+function fetchSuccess() {
+  return Promise.resolve({
     ok: true,
-    json: jest.fn().mockResolvedValue(mockpost)
-});
-
-global.fetch = mockfetch;
+    status: 200,
+    statusText: "OK",
+    json: () => Promise.resolve(mockpost),
+  });
+}
 
 describe("create", () => {
-    it("returns a valid item with a valid input", async () => {
-        const post = await createPost("title", ["tag1", "tag2"], "", "body");
-        expect(post).toMatchObject(mockpost);
-    });
+  it("returns a valid item with a valid input", async () => {
+    global.fetch = jest.fn(() => fetchSuccess());
+    const post = await createPost("title", ["tag1", "tag2"], "", "body");
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });
